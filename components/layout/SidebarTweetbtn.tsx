905879650e6fb081b63f8f9b49a0757cc6d5265f
Your Name <sanjayshreeram77@gmail.com>
2024-04-13 16:37:09 +0530

    first commit

--- components/layout/SidebarTweetbtn.tsx
+++ components/layout/SidebarTweetbtn.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import { FaFeather } from 'react-icons/fa';
+import { UserLoginModal } from '../hooks/UserLoginModal';
 
 const SidebarTweetbtn: React.FC = () => {
+  const loginmodal=UserLoginModal();
     const router = useRouter();
     return (
-        <div  onClick={()=>router.push('/')}>
+        <div  onClick={()=>loginmodal.onopen()}>
          <div className='mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex items-center justify-center bg-sky-500 hover:bg-opacity-80 transition cursor-pointer'>
        <FaFeather/>
          </div>
