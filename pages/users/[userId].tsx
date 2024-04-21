5b4b69caa4f0321e2044d010b005368c1b45c389
Your Name <sanjayshreeram77@gmail.com>
2024-04-21 18:30:03 +0530

    first commit

--- pages/users/[userId].tsx
+++ pages/users/[userId].tsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import Header from '@/components/Header'
+import { useRouter } from 'next/router'
+import { useUser } from '@/components/hooks/useUser';
+import { ClipLoader } from 'react-spinners';
+import UserHero from '@/components/users/UserHero';
 
 const UserView= () => {
+    const router=useRouter();
+    const {userId}=router.query;
+
+    const  {data:fetchedUser,isLoading}=useUser(userId as string)
+
+    if(isLoading || !fetchedUser)
+        {
+            return (
+                <div className='flex justify-center  items-center h-full '>
+                    <ClipLoader color='lightblue' size={80}/>
+                    </div>
+            )
+        }
+
   return (
-    <div>[userId]</div>
+    <>
+    <Header showBackArrow label={fetchedUser?.name}/>
+    <UserHero userId={userId as string}/>
+    
+    </>
   )
 }
 
