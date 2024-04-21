0aa0dca0ae4dc8fe7c6b436c6eb5cde76599dc62
Your Name <sanjayshreeram77@gmail.com>
2024-04-21 22:15:28 +0530

    first commit

--- components/Button.tsx
+++ components/Button.tsx
@@ -14,7 +14,7 @@ interface ButtonProps {
 
 const Button:React.FC<ButtonProps> = ({label,secondary,fullWdith,large,onClick,disabled,outline}) => {
   return ( 
-    <button onClick={onClick} disabled={disabled} className={`disabled:opacity-70 disabled:cursor-not-allowed  rounded-full font-semibold hover:opacity-80 transition border-2  ${fullWdith?'w-full':'w-fit'} ${secondary ?'bg-white':'bg-sky-500'} ${secondary ?'text-white':'text-black'} ${secondary?'border-black':'border-sky-500'} ${large?'text-xl':'text-md'} ${outline?'bg-transparent':''} ${outline?'border-white':''} ${outline?'text-white':''}`}>
+    <button onClick={onClick} disabled={disabled} className={`disabled:opacity-70 disabled:cursor-not-allowed  rounded-full font-semibold hover:opacity-80 transition border-2  ${fullWdith?'w-full':'w-fit'} ${secondary ?'bg-white':'bg-sky-500'} ${secondary ?'text-black':'text-white'} ${secondary?'border-black':'border-sky-500'} ${large?'text-xl':'text-md'} ${outline?'bg-transparent':''} ${outline?'border-white':''} ${outline?'text-white':''}`}>
     {label}
     </button>
   )
