c5f4f4d8dd014a5cb6f59dd252148cfe9d02004b
Your Name <sanjayshreeram77@gmail.com>
2024-04-22 22:16:44 +0530

    first commit

--- components/modals/Editmodal.tsx
+++ components/modals/Editmodal.tsx
@@ -1,8 +1,10 @@
-import { use, useEffect } from "react";
+import { use, useCallback, useEffect } from "react";
 import { usecurrentUser } from "../hooks/UseCurrentUser";
 import { useUser } from "../hooks/useUser";
 import { UserEditModal } from "../hooks/UserEditModal";
 import { useState } from "react";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 const Editmodal = () => {   
 
@@ -10,7 +12,7 @@ const Editmodal = () => {
     const {mutate:mutateFetchUser}=useUser(currentUser?.id);
     const editmodal=UserEditModal();
 
-    const [profileImage,setProfileImage]=useState('');
+   
     const [coverImage,setCoverImage]=useState('');
     const [name,setName]=useState('');
     const [username,setUsername]=useState('');
@@ -23,11 +25,40 @@ const Editmodal = () => {
             setName(currentUser.name);
             setUsername(currentUser.username);
             setBio(currentUser.bio);
-            setProfileImage(currentUser.profileImage);
+          
             setCoverImage(currentUser.coverImage);
         }
 
-     },[])
+     },[currentUser])
+
+     const [isLoading,setIsLoading]=useState(false);
+
+    const onsubmit=useCallback(async ()=>{
+        try {
+            setIsLoading(true);
+
+            await axios.patch('/api/Edit',{
+                name,
+                username,
+                bio,
+                coverImage
+
+            }
+
+            )
+
+
+        }
+        catch(error)
+        {  toast.error('An error occured');
+           
+
+        }
+        finally {
+            setIsLoading(false);
+        }
+
+    },[])
 
     return (
         <div>
